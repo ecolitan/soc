633a813af789ab27e0b32e15c134352c4af5a468
Aaron Cossey <aaron.cossey@gmail.com>
2014-02-13 19:52:08 +0100

    updates to testserver and client

--- gameclient.py
+++ gameclient.py
@@ -1,21 +1,47 @@
 import socket
 import sys
+import uuid
+import json
+    
+class GameClient:
+    """example client"""
+    
+    def __init__(self):
+        # Each client has a unique id string
+        self.client_id = str(uuid.uuid4())
+        #TODO client config file
+        self.HOST = "localhost"
+        self.PORT = 9999
+        # Create a socket (SOCK_STREAM means a TCP socket)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-HOST, PORT = "localhost", 9999
-data = " ".join(sys.argv[1:])
+    def send_request(self, request):
+        """Send request to gameserver
+        Return reply
+        """
+        
+        try:
+            # Connect to server and send data
+            self.sock.connect((self.HOST, self.PORT))
+            self.sock.sendall(request)
 
-# Create a socket (SOCK_STREAM means a TCP socket)
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            # Receive data from the server and shut down
+            received = self.sock.recv(1024)
+        finally:
+            self.sock.close()
 
-try:
-    # Connect to server and send data
-    sock.connect((HOST, PORT))
-    sock.sendall(data + "\n")
+        print "Sent:     {}".format(request)
+        print "Received: {}".format(received)
+        
+        return received
+        
+    def generate_request(self):
+        """generate a request"""
+        return str({"uuid", self.client_id})
 
-    # Receive data from the server and shut down
-    received = sock.recv(1024)
-finally:
-    sock.close()
-
-print "Sent:     {}".format(data)
-print "Received: {}".format(received)
+if __name__ == "__main__":
+    client = GameClient()
+    request = client.generate_request()
+    client.send_request(request)
+    
+        
--- gameserver.py
+++ gameserver.py
@@ -1,23 +1,25 @@
 import SocketServer
+import uuid
+from player import BasePlayer
+from clientrequest import ClientRequest
 
 class MyTCPHandler(SocketServer.StreamRequestHandler):
 
     def handle(self):
-        # self.rfile is a file-like object created by the handler;
-        # we can now use e.g. readline() instead of raw recv() calls
-        self.data = self.rfile.readline().strip()
-        print "{} wrote:".format(self.client_address[0])
-        print self.data
-        # Likewise, self.wfile is a file-like object used to write back
-        # to the client
-        self.wfile.write(self.data.upper())
+        #~ self.data = self.rfile.readline().strip()
+        self.data = self.request.recv(1024).strip()
+        
+        #~ self.wfile.write(self.data.upper())
+        self.request.sendall(self.data.upper())
         
 if __name__ == "__main__":
     HOST, PORT = "localhost", 9999
-
+    a = 0
     # Create the server, binding to localhost on port 9999
     SocketServer.TCPServer.allow_reuse_address = True
     server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+
+    player_list =  []
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
