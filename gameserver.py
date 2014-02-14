474e5648ebdfa6ea1791942717d007ff3a1143ee
Aaron Cossey <aaron.cossey@gmail.com>
2014-02-14 11:18:20 +0100

    server threading

--- gameserver.py
+++ gameserver.py
@@ -14,7 +14,6 @@ class ThreadedTCPRequestHandler(SocketServer.StreamRequestHandler):
                 cur_thread = threading.current_thread()
                 response = "{}: {}".format(cur_thread.name, self.data)
                 self.request.sendall(response)
-            #~ time.sleep(1)    
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     pass
@@ -23,12 +22,7 @@ if __name__ == "__main__":
     HOST, PORT = "localhost", 9999
     server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
     ip, port = server.server_address
-
-    # Start a thread with the server -- that thread will then start one
-    # more thread for each request
     server_thread = threading.Thread(target=server.serve_forever())
-    # Exit the server thread when the main thread terminates
     server_thread.daemon = True
     server_thread.start()
-    print "Server loop running in thread:", server_thread.name
     
